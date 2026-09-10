30745c693e503e7b071fb8d8f9a614d7ec052c9f
Peter Pfeufer <info@ppfeufer.de>
2026-09-10 17:02:22 +0200

    [RELEASE] v0.0.3
    ## [0.0.3] - 2026-09-10
    
    ### Changed
    
    - Provide defaults as function instead of an object. This seems a bit more elegant and
      allows us to play more dynamically with the defaults if needed in the future.
    - Make it a proper DataTables Extension

--- src/datatables-filterdropdown.js
+++ src/datatables-filterdropdown.js
@@ -1,7 +1,7 @@
 /*!
  * DataTables - filterDropDown plugin (modernized fork by Peter Pfeufer)
  *
- * @version 0.0.1
+ * @version 0.0.3
  * @author Peter Pfeufer
  * @license GPL-3.0 or later
  * @link https://github.com/ppfeufer/datatables-filterdropdown
@@ -68,7 +68,7 @@
 })((window, document, DataTable) => { // jshint ignore:line
     'use strict';
 
-    const version = '0.0.2';
+    const version = '0.0.3';
 
     /**
      * Default settings for the filterDropDown plugin.
